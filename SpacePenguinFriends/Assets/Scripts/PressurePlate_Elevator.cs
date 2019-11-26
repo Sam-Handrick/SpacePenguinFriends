949fd02ecd795e2a5e832c939ece9c652fa20bfc
snguyen801 <45181789+snguyen801@users.noreply.github.com>
2019-11-26 11:26:01 -0800

    Update PressurePlate_Elevator.cs to check tags for Player or other objects. Public array available to add additional tags too.

--- SpacePenguinFriends/Assets/Scripts/PressurePlate_Elevator.cs
+++ SpacePenguinFriends/Assets/Scripts/PressurePlate_Elevator.cs
@@ -4,23 +4,39 @@ using UnityEngine;
 
 public class PressurePlate_Elevator : MonoBehaviour
 {
-    public GameObject door;
+    //public GameObject elevator;
     public Animator anim;
+    public string[] elevatorTag = new string[10];
 
-    void OnTriggerEnter(Collider col)
-    {
-        //door.transform.position += new Vector3(0, 4, 0);
-        anim.SetTrigger("Lift");      
-    }
 
-    void OnTriggerExit(Collider col)
-    {
-        //door.transform.position += new Vector3(0, -4, 0);
-        anim.enabled = true;
-    }
-
+//Used along with OnTriggerEnter to disable/pause the elevator animation at the top
     void pauseAnimationEvent()
     {
         anim.enabled = false;
     }
+
+//checks if the Collider's tag is in the elevatorTag array when a GameObject (w/Collider) enters the collider trigger
+    void OnTriggerEnter(Collider col)
+    {
+        //Debug.Log(col.tag.GetType());
+
+        foreach(string eTag in elevatorTag)
+        {
+            //if (col.tag == "Player" || col.tag == elevatorTag)
+            if (col.tag == eTag)
+            {
+                //elevator.transform.position += new Vector3(0, 4, 0);
+                anim.SetTrigger("Elevator_Lift");      
+            }
+        }
+    }
+
+//Re-enables/resumes the elevator animation which drops back down when the GameObject exits the collider
+    void OnTriggerExit(Collider col)
+    {
+        //elevator.transform.position += new Vector3(0, -4, 0);
+        anim.enabled = true;
+    }
+
+
 }
