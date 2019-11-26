115c9025946f609616fd89badeccc80020cc01ce
snguyen801 <45181789+snguyen801@users.noreply.github.com>
2019-11-25 17:38:53 -0800

    Adding Pressure Plate script & testbed, and door animation & animator.

---
+++ SpacePenguinFriends/Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public GameObject door;
+    public Animator anim;
+
+    void OnTriggerEnter(Collider col)
+    {
+        //door.transform.position += new Vector3(0, 4, 0);
+        anim.SetTrigger("DoorOpen");      
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        //door.transform.position += new Vector3(0, -4, 0);
+        anim.enabled = true;
+    }
+
+    void pauseAnimationEvent()
+    {
+        anim.enabled = false;
+    }
+}
