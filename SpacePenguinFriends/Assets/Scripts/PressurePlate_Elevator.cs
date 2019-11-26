6e64f1bac26907e29ccc111f8914918f6a35ab41
snguyen801 <45181789+snguyen801@users.noreply.github.com>
2019-11-26 15:37:46 -0800

    PressurePlate animates through code and has public variables for max/min Height, and rate of ascending and descent.

--- SpacePenguinFriends/Assets/Scripts/PressurePlate_Elevator.cs
+++ SpacePenguinFriends/Assets/Scripts/PressurePlate_Elevator.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class PressurePlate_Elevator : MonoBehaviour
 {
-    //public GameObject elevator;
-    public Animator anim;
-    //Old Array for elevatorTag (new List below) -> //public string[] elevatorTag = new string[10];
+    public GameObject elevator;
+    public float maxHeight = 5;
+    public float minHeight = 1;
+    public float rateUp = 0.1f;
+    public float rateDown = 0.2f;
+    private bool tagEntered = false;
     public List<string> elevatorTag = new List<string>(new string[] {"Player"});
     
-
-
-//Used along with OnTriggerEnter to disable/pause the elevator animation at the top
-    void pauseAnimationEvent()
+    //public Animator anim;
+    //Old Array for elevatorTag (new List below) -> //public string[] elevatorTag = new string[10];
+    
+    void goingUp()
     {
-        anim.enabled = false;
+        elevator.transform.position += new Vector3(0, rateUp, 0);
     }
+    void goingDown()
+    {
+        elevator.transform.position -= new Vector3(0, rateDown, 0);
+    }
+
+    /*
+    void Start()
+    {
+        minHeight = elevator.transform.position.y;
+    }
+    */
 
 //checks if the Collider's tag is in the elevatorTag array when a GameObject (w/Collider) enters the collider trigger
     void OnTriggerEnter(Collider col)
@@ -23,20 +37,43 @@ public class PressurePlate_Elevator : MonoBehaviour
         //Debug.Log(col.tag.GetType());
         foreach(string eTag in elevatorTag)
         {
-            if (col.tag == eTag || col.tag == "Player")
+            if (col.tag == eTag)
             {
-                //elevator.transform.position += new Vector3(0, 4, 0);
-                anim.SetTrigger("Elevator_Lift");      
+                tagEntered = true;
+                //anim.SetTrigger("Elevator_Lift");      
             }
         }
     }
 
-//Re-enables/resumes the elevator animation which drops back down when the GameObject exits the collider
-    void OnTriggerExit(Collider col)
+    void OnTriggerStay(Collider col)
     {
-        //elevator.transform.position += new Vector3(0, -4, 0);
-        anim.enabled = true;
+        Debug.Log(elevator.transform.position.y);
+        if (tagEntered == true && elevator.transform.position.y < maxHeight)
+        {
+            goingUp();
+        }
     }
 
+    void OnTriggerExit(Collider col)
+    {
+        tagEntered = false;
+        
+        //anim.enabled = true;   
+            //Re-enables/resumes the elevator animation which drops back down when the GameObject exits the collider
+    }
+    void Update()
+    {
+        if (tagEntered == false && elevator.transform.position.y > minHeight)
+        {
+            goingDown();
+        }
+    }
+
+    /*//Used along with OnTriggerEnter to disable/pause the elevator animation at the top
+    void pauseAnimationEvent()
+    {
+        anim.enabled = false;
+    }
+    */
 
 }
